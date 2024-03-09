eb34cbcffa8ea9f94dea6a7663f352b2ec03eed0
changuk2 <changuk0308@naver.com>
2024-03-09 23:26:18 +0900

    Test2

--- taba_server/src/main/java/jpabook/jpashop/JpashopApplication.java
+++ taba_server/src/main/java/jpabook/jpashop/JpashopApplication.java
@@ -7,8 +7,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class JpashopApplication {
 
 	public static void main(String[] args) {
-
-		System.out.println("안녕하세요 타바 팀 입니다.");
 		SpringApplication.run(JpashopApplication.class, args);
 	}
 }
