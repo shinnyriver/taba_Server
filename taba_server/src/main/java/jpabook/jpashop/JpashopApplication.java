79330b59771290442f12ee367e66a2fc63c9d854
changuk2 <changuk0308@naver.com>
2024-03-09 23:28:41 +0900

    TEST2

--- taba_server/src/main/java/jpabook/jpashop/JpashopApplication.java
+++ taba_server/src/main/java/jpabook/jpashop/JpashopApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class JpashopApplication {
 
 	public static void main(String[] args) {
+		System.out.println("test2");
 		SpringApplication.run(JpashopApplication.class, args);
 	}
 }
