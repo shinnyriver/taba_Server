d9d2da49d7af3d085b763e74bc420ba79aa48aac
Changsu Park <gd12091@gmail.com>
2024-04-19 21:50:42 +0900

    Feat: Response & Request Dto

---
+++ taba_server/src/main/java/taba/tabaServer/tabaserver/repository/UserRepository.java
@@ -0,0 +1,10 @@
+package taba.tabaServer.tabaserver.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import taba.tabaServer.tabaserver.domain.User;
+
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findByEmail(String email);
+}
