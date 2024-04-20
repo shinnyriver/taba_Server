5dcad4b0b12fa4ab938f7df8c0b2133eb545ba7c
Changsu Park <gd12091@gmail.com>
2024-04-20 18:42:34 +0900

    Feat: update Car Method

--- taba_server/src/main/java/taba/tabaServer/tabaserver/repository/CarRepository.java
+++ taba_server/src/main/java/taba/tabaServer/tabaserver/repository/CarRepository.java
@@ -3,5 +3,8 @@ package taba.tabaServer.tabaserver.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import taba.tabaServer.tabaserver.domain.Car;
 
+import java.util.Optional;
+
 public interface CarRepository extends JpaRepository<Car, Long> {
+    Optional<Car> findAllByUserId(Long user_id);
 }
