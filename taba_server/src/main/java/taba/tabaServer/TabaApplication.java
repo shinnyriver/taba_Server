0c85e17404f730a76e3aa9188236335314120e1d
Changsu Park <gd12091@gmail.com>
2024-05-16 19:20:33 +0900

    feat: JWT Secret key with YAML

--- taba_server/src/main/java/taba/tabaServer/TabaApplication.java
+++ taba_server/src/main/java/taba/tabaServer/TabaApplication.java
@@ -2,8 +2,11 @@ package taba.tabaServer;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import taba.tabaServer.tabaserver.security.config.JwtProperties;
 
 @SpringBootApplication
+@EnableConfigurationProperties(JwtProperties.class)
 public class TabaApplication {
 
 	public static void main(String[] args) {
