58840d02ccd9508cc327727be12d913ae00ebd0c
Changsu Park <gd12091@gmail.com>
2024-06-02 21:30:07 +0900

    refactor : reformatted

--- taba_server/src/main/java/taba/tabaServer/TabaApplication.java
+++ taba_server/src/main/java/taba/tabaServer/TabaApplication.java
@@ -4,7 +4,6 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import taba.tabaServer.tabaserver.security.config.JwtProperties;
-//import taba.tabaServer.tabaserver.security.config.JwtProperties;
 
 @SpringBootApplication
 @EnableConfigurationProperties(JwtProperties.class)
