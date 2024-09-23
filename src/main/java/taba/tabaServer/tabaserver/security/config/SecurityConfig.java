32eec5d0c89294dca730a617b89d8284216828a0
Changsu Park <gd12091@gmail.com>
2024-09-23 14:41:56 +0900

    fix : security

--- src/main/java/taba/tabaServer/tabaserver/security/config/SecurityConfig.java
+++ src/main/java/taba/tabaServer/tabaserver/security/config/SecurityConfig.java
@@ -26,28 +26,30 @@ public class SecurityConfig {
     }
 
     @Bean
-    public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity, JwtRequestFilter jwtRequestFilter) throws Exception {
+    public SecurityFilterChain securityFilterChain(
+            HttpSecurity httpSecurity, JwtRequestFilter jwtRequestFilter) throws Exception {
         return httpSecurity
                 .httpBasic().disable() // HTTP 기본 인증 비활성화
                 .csrf().disable() // CSRF 보호 비활성화
                 .cors().configurationSource(corsConfigurationSource()) // CORS 설정 활성화
                 .and()
                 .authorizeRequests() // 요청에 대한 보안 검사 시작
-                .requestMatchers("/api/**").permitAll() // "/api/**" 경로의 모든 요청을 인증 없이 허용
+                .requestMatchers("/api/oauth/**", "/api/managers/login", "/api/managers/register").permitAll() // 인증과 관련된 경로는 허용 (로그인, 회원가입 등)
+                .requestMatchers("/api/**").authenticated() // "/api/**" 경로의 요청은 JWT 인증이 필요
                 .anyRequest().authenticated() // 그 외의 모든 요청은 인증 필요
                 .and()
                 .sessionManagement()
                 .sessionCreationPolicy(SessionCreationPolicy.STATELESS) // 세션 상태를 STATELESS로 설정 (주로 JWT 사용시)
                 .and()
-                .addFilterBefore(jwtRequestFilter, UsernamePasswordAuthenticationFilter.class)
+                .addFilterBefore(jwtRequestFilter, UsernamePasswordAuthenticationFilter.class) // JWT 필터 추가
                 .build();
     }
 
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(List.of("https://api.ice-taba.site", "https://www.ice-taba.site")); // 모든 도메인에서의 접근을 허용
-        configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS")); // 허용할 HTTP 메소드 지정
+        configuration.setAllowedOrigins(List.of("https://api.ice-taba.site", "https://www.ice-taba.site")); // 허용할 도메인 설정
+        configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS")); // 허용할 HTTP 메소드 설정
         configuration.setAllowedHeaders(List.of("*")); // 모든 헤더 허용
         configuration.setAllowCredentials(true); // 크리덴셜 허용
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
